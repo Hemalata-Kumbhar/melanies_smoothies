8d3c0788ae1d4585ac132b1e7b10a7175abd5979
Hemalata-Kumbhar <hemalata.kumbhar@vanquis.com>
2026-08-27 23:11:14 +0100

    Integrate SmoothieFroot API for fruit options
    Added requests import and API call to fetch fruit data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
+
 
 # Write directly to the app
 st.title(f"🥤 Customize Your Smoothie :🥤:")
@@ -15,8 +17,9 @@ name_on_order = st.text_input('Name on smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -30,6 +33,9 @@ if ingredients_list:
 
                 for fruit_chosen in ingredients_list:
                     ingredients_string += fruit_chosen + ' '
+                    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+                    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+                    
 
                 #st.write(ingredients_string)
 
@@ -46,8 +52,5 @@ if ingredients_list:
                     st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-                    
+
+
